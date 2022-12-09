8da57bd6f4b7058844e6ac1b75ae3ecf48627347
Kang Min Woo <mynooo7688@gmail.com>
2022-12-09 20:47:15 +0900

    Send data page to page

--- ScreenTransitionExample/CodePresentViewController.swift
+++ ScreenTransitionExample/CodePresentViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class CodePresentViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     @IBAction func tapBackButton(_ sender: UIButton) {
         self.presentingViewController?.dismiss(animated: true, completion: nil )
     }
+    
 }
--- ScreenTransitionExample/CodePushViewController.swift
+++ ScreenTransitionExample/CodePushViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class CodePushViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    var name: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     @IBAction func tapBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+    
 }
