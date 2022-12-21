814a34244627002bed1028de92db5191fc172609
MinWoo <mynooo7688@gmail.com>
2022-12-22 01:48:29 +0900

    계산기 완성

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -39,24 +39,75 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOperation = .unknown
+        self.numberOutputLabel.text = "0"
     }
     
     @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains("."){
+        self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+        self.numberOutputLabel.text = self.displayNumber
+        }
     }
     
     @IBAction func tapDivideButton(_ sender: UIButton) {
+        self.operation(.Divide)
     }
     
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        self.operation(.Multiply)
     }
     
     @IBAction func tapSubtractButton(_ sender: UIButton) {
+        self.operation(.Subtract)
     }
     
     @IBAction func tapAddButton(_ sender: UIButton) {
+        self.operation(.Add)
     }
     
     @IBAction func tapEqualButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return}
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                    
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                
+                default:
+                    break
+                }
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            
+            self.currentOperation = operation
+        } else {
+          self.firstOperand = self.displayNumber
+          self.currentOperation = operation
+          self.displayNumber = ""
+        }
+        
     }
 }
 
