8eca3668ec323520b5078a6ab50ee3483a2614af
MinWoo <mynooo7688@gmail.com>
2022-12-23 14:10:33 +0900

    Initial Commit

---
+++ whattodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whattodo
+//
+//  Created by 강민우 on 2022/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
