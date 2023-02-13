6e315f60f217a395c22d9b1a99201363bce37335
MinWoo <mynooo7688@gmail.com>
2023-02-13 21:15:47 +0900

    covid19 현황판 만들기

--- Covid19/ViewController.swift
+++ Covid19/ViewController.swift
@@ -5,15 +5,18 @@
 //  Created by 강민우 on 2023/02/13.
 //
 
+import Charts
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
