e2cb86480429240605b09d8642064af299854ccb
MinWoo <mynooo7688@gmail.com>
2023-02-13 14:35:32 +0900

    Initial Commit

---
+++ Covid19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid19
+//
+//  Created by 강민우 on 2023/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
