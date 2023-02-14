4cb4db79c9abe6f05b69f275ab6c36ad475ddc88
MinWoo <mynooo7688@gmail.com>
2023-02-14 23:36:49 +0900

    api 호출기능 구현

--- Covid19/CityCovidOverView.swift
+++ Covid19/CityCovidOverView.swift
@@ -7,3 +7,32 @@
 
 import Foundation
 
+struct CityCovidOverview: Codable {
+    let korea: CovidOverview
+    let seoul: CovidOverview
+    let busan: CovidOverview
+    let daegu: CovidOverview
+    let incheon: CovidOverview
+    let gwangju: CovidOverview
+    let ulsan: CovidOverview
+    let sejong: CovidOverview
+    let gangwon: CovidOverview
+    let chungbuk: CovidOverview
+    let chungnam: CovidOverview
+    let jeonbuk: CovidOverview
+    let jeonnam: CovidOverview
+    let gyeongbuk: CovidOverview
+    let gyeongnam: CovidOverview
+    let jeju: CovidOverview
+} //구조체 정의
+
+struct CovidOverview: Codable {
+    let countryName: String
+    let newCase: String
+    let totalCase: String
+    let recovered: String
+    let death: String
+    let percentage: String
+    let newCcase: String
+    let newFcase: String
+}
