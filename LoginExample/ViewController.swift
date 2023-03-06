1d3ec8b67685f800ccd7154a55e614205d311477
MinWoo <mynooo7688@gmail.com>
2023-03-06 23:04:45 +0900

    loginexample

--- LoginExample/ViewController.swift
+++ LoginExample/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var GoogleLoginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func GoogleLoginButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
